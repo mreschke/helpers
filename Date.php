f6c4996da09398e383ded028ccbb8020f7ac9784
Matthew Reschke <mail@mreschke.com>
2017-10-25 13:21:01 -0600

    Added new date splitDays method

--- Date.php
+++ Date.php
@@ -1,5 +1,7 @@
 <?php namespace Mreschke\Helpers;
 
+use Carbon\Carbon;
+
 /**
  * Date helpers.
  * @copyright 2014 Matthew Reschke
@@ -24,11 +26,11 @@ class Date
             $utimestamp = microtime();
         }
         list($usec, $epoch) = explode(" ", microtime());
-        
+
         # Get microseconds (millionths) and guarantee 6 characters
         $usec = $usec * 1000000;
         $usec = str_pad($usec, 6, "0", STR_PAD_LEFT);
-        
+
         # Find milliseconds (thousands) and guarantee 3 characters
         $msec = round($usec / 1000); #milliseconds (thousands)
         $msec = str_pad($msec, 3, "0", STR_PAD_LEFT);
@@ -40,4 +42,27 @@ class Date
 
         return date($format, $epoch);
     }
+
+    /**
+     * Split date range into chunks by $days (inclusive enddates 23:59:59)
+     * @param  Carbon\Carbon $startDate
+     * @param  Carbon\Carbon $endDate
+     * @param  int $days = 30
+     * @return array
+     */
+    public static function splitDays($startDate, $endDate, $days = 30)
+    {
+        $dates = [];
+        $maxDate = $endDate; $endDate = $startDate;
+        while ($endDate < $maxDate) {
+            $endDate = (new Carbon($startDate))->addDays($days-1)->endOfDay();
+            if ($endDate > $maxDate) $endDate = $maxDate->copy()->endOfDay();
+            $dates[] = ['start' => $startDate->copy(), 'end' => $endDate->copy()];
+            $startDate = $endDate->copy()->addSecond();
+        }
+        return $dates;
+    }
+
+
 }
+
